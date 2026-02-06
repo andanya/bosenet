65a7f076ac34a750d9c2d273338016a312298b38
Daniil Antonenko <da753@r904u10n01.grace.ycrc.yale.edu>
2026-02-06 01:53:35 -0500

    Corrected bash script for optimization

--- run-psiformer.sh
+++ run-psiformer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=andanya-psiformer
 #SBATCH --partition=gpu
-#SBATCH --gpus=2
+#SBATCH --gpus=1
 #SBATCH --time=12:00:00
 #SBATCH --mem=16G
 #SBATCH --output=./andanya-psiformer-jobs/andanya-psiformer-job_%j.out
